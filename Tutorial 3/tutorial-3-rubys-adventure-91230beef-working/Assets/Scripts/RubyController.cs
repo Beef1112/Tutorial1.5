18d140245e10373be83030af374d44a96ed0d29e
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 02:04:55 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -8,7 +8,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
