9e98f3a6867561f829def3c18303c8529fe5f1b2
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 02:47:28 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public int maxHealth = 5;
-    int currentHealth
+    int currentHealth;
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
