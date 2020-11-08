51730c8b208773d0bc05180599830f43d1070a1c
91230jack@gmail.com <91230jack@gmail.com>
2020-11-07 23:12:25 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -8,7 +8,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         QualitySettings.vSyncCount = 0;
-        
+        Application
     }
 
     // Update is called once per frame
