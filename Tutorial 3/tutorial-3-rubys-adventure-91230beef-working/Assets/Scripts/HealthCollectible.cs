c1c1feee622b88f7ab5c98239384923d45a9ea50
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 03:25:48 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\HealthCollectible.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/HealthCollectible.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/HealthCollectible.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
-public class HealthCollectible : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
