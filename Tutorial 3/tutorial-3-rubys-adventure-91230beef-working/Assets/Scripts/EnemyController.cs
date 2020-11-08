f952ece33a70beb985b6cec57751cb13c3b1e87a
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 09:08:51 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\EnemyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/EnemyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/EnemyController.cs
@@ -17,7 +17,7 @@ public class EnemyController : MonoBehaviour
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
-        Animator = GetComponent<Animator>()
+        Animator = GetComponent<Animator>();
     }
 
     void Update()
