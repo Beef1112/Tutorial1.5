b124e170b548e5980b803a4389ba3c383d4ebdca
91230jack@gmail.com <91230jack@gmail.com>
2020-11-07 23:12:13 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        quality
+        QualitySettings
     }
 
     // Update is called once per frame
