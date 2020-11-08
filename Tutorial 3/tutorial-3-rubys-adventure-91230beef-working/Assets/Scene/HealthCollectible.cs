a900e99abf1eb181a2e534e3875f2d39f5da373a
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 03:11:09 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scene\HealthCollectible.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scene/HealthCollectible.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scene/HealthCollectible.cs
@@ -6,10 +6,10 @@ public class HealthCollectible : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-    RubyController controller = other.GetComponent<RubyController>();
+        RubyController controller = other.GetComponent<RubyController>();
 
-    if (controller != null)
-    {
+        if (controller != null)
+        {
         controller.ChangeHealth(1);
         Destroy(gameObject);
     }
