a58a71524070d16f971638a02955b6210294403f
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 03:03:51 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scene\HealthCollectible.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scene/HealthCollectible.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scene/HealthCollectible.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-void OnTriggerEnter
+void OnTriggerEnter2D
     // Start is called before the first frame update
     void Start()
     {
