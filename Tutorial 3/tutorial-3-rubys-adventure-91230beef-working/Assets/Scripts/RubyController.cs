7aec22716f68152a87f524b10c03208ca8ace937
91230jack@gmail.com <91230jack@gmail.com>
2020-11-07 23:11:28 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -18,7 +18,7 @@ public class RubyController : MonoBehaviour
         Debug.Log(horizontal);
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
-        position.y = position.y +
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
