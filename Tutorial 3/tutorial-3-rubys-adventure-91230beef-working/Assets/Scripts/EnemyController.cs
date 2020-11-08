834408e82ec3debfc48f474ef063f9f3c9c8daf7
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 04:31:27 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\EnemyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/EnemyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/EnemyController.cs
@@ -10,7 +10,7 @@ public class EnemyController : MonoBehaviour
     
     Rigidbody2D rigidbody2D;
     float timer;
-    
+    int direction = 1
     
     // Start is called before the first frame update
     void Start()
