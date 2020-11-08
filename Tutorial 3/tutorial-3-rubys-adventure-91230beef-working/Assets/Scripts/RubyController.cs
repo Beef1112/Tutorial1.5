779efc0a520ce6fd25edd9de69609e1b7d6afa24
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 14:41:14 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -19,7 +19,8 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
-    Animator animator
+    Animator animator;
+    Vector2
 
     // Start is called before the first frame update
     void Start()
