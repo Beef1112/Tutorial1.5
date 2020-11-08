93d5a73ec6670c08a534611681494e31618d1654
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 14:43:05 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -39,7 +39,7 @@ public class RubyController : MonoBehaviour
 
         Vector2 move = new Vector2(horizontal, vertical);
 
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately )
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move))
 
         if (isInvincible)
         {
