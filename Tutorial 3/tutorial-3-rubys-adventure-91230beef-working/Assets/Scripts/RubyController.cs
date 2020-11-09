040681ce0bdf71a8d640eb7672f2ad1dd3b18b4b
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 23:15:43 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -83,7 +83,6 @@ public class RubyController : MonoBehaviour
             }
         }
 
-        function Update () {
              if (Input.GetKey ("escape")) {
                  Application.Quit();
                 }
