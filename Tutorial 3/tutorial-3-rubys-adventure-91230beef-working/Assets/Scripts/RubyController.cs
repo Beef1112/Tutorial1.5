17ea40d6b1998de76dbb0b660cb60f063d6ca159
91230jack@gmail.com <91230jack@gmail.com>
2020-11-08 23:02:58 -0500

    Modified c:\Users\91230\OneDrive\Desktop\Tutorial 3\tutorial-3-rubys-adventure-91230beef-working\Assets\Scripts\RubyController.cs

--- Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
+++ Tutorial 3/tutorial-3-rubys-adventure-91230beef-working/Assets/Scripts/RubyController.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-
+    
     public int maxHealth = 5;
-
+    
     public GameObject projectilePrefab;
-
-    public float timeInvincible = 2.0f;
-
+    
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+    
     public int health { get { return currentHealth; }}
     int currentHealth;
-
+    
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
-
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
+    
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
-
+    
+    AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
+        
         currentHealth = maxHealth;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -39,31 +45,31 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-
+        
         Vector2 move = new Vector2(horizontal, vertical);
-
+        
         if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
-
+        
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-
+        
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
-
+        
         if(Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
-
+        
         if (Input.GetKeyDown(KeyCode.X))
         {
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
@@ -77,12 +83,12 @@ public class RubyController : MonoBehaviour
             }
         }
     }
-
+    
     void FixedUpdate()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
@@ -91,19 +97,20 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
-            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            
+            PlaySound(hitSound);
         }
-
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
-
+    
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
@@ -112,5 +119,12 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        
+        PlaySound(throwSound);
+    } 
+    
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
